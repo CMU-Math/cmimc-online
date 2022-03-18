5211eff409c0b1f6449a3e545d4ab04dbbf4b3b8
Chris Lambert <cslamber@andrew.cmu.edu>
2022-03-18 13:22:18 -0400

    asdf

--- website/tasks.py
+++ website/tasks.py
@@ -255,11 +255,11 @@ def check_graded_submissions():
         ts = TaskScore.objects.get(task=t, score=s)
         if sub.points is None:
             continue
-        if sub.points > ts.raw_points or ts_raw_points is None:
+        if ts.raw_points is None or sub.points > ts.raw_points:
             ts.raw_points = sub.points
             ts.save()
 
-            if ts.raw_points > t.best_raw_points or t.best_raw_points is None:
+            if t.best_raw_points is None ts.raw_points > t.best_raw_points:
                 t.best_raw_points = ts.raw_points
                 t.save()
                 for ts2 in t.taskscores.all():
